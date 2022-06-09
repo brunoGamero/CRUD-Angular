62b378b08b314c99b8b2fcedcd538a241f9bab90
Bruno Gama <brunoindie\2gmail.com>
2022-06-09 11:31:34 -0300

    Primeiro commit

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
+import { CoursesRoutingModule } from './courses-routing.module';
+import CursosComponent from './courses/cursos.component';
+
+
+
+@NgModule({
+  declarations: [
+    CursosComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule,
+    SharedModule
+  ]
+})
+export class CoursesModule { }
---
+++ src/app/courses/courses/cursos.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export default class CursosComponent implements OnInit {
+
+courses$: Observable<Course[]>;
+displayedColumns = ['name', 'category'];
+
+  // coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService,
+              public dialog: MatDialog) {
+
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos!');
+        return of([])
+      })
+    );
+
+
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+  });
+}
+
+  ngOnInit(): void {
+
+
+}
+
+}
